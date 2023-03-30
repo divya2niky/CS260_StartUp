814ff28913c9f158dba8ee9ceb86470b75a389b3
divya2niky <divya2niky@gmail.com>
2023-03-29 20:04:20 -0600

    updated readme with simon-websocket notes

---
+++ StartUp/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app =express();
+const DB = require('./database.js');
+
+var apiRouter = express.Router();
+app.use('/api',apiRouter);
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
